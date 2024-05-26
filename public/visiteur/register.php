f6449f799996b8a62a42e800033c97ae2a8f43db
Yun000 <yuness.smail9@gmail.com>
2024-05-26 23:07:00 +0200

    register changes

--- public/visiteur/register.php
+++ public/visiteur/register.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cupid Quest</title>
-    <link rel="stylesheet" href="register.css"> <!-- peut etre à retirer-->
     <link rel="stylesheet" href="../../src/element/header.css">
+    <link rel="stylesheet" href="register.css">
 </head>
 
 <body>
@@ -21,11 +21,11 @@
     ?>
 
     <form action="" method="post">
-        <label>Pseudonyme</label> <input maxlength="50" placeholder="Votre pseudonyme" name="id" required>
+        <label>Pseudonyme</label> <input type="text" placeholder="Votre pseudo" name="id" required>
         <br>
-        <label>Nom</label> <input maxlength="50" placeholder="Votre nom" name="lastname" required>
+        <label>Nom</label> <input type="text" placeholder="Votre nom" name="lastname" required>
         <br>
-        <label>Prénom</label> <input maxlength="50" placeholder="Votre prénom" name="name" required>
+        <label>Prénom</label> <input type="text" placeholder="Votre prénom" name="name" required>
         <br>
         <label>Genre</label> <select name="gender">
             <option value="Homme">Homme</option>
@@ -36,18 +36,17 @@
         <br>
         <label>E-Mail</label> <input type="email" placeholder="Enter email" name="email" required>
         <br>
-        <label>Mot de passe</label> <input type="password" maxlength="50" placeholder="Enter password" name="password"
-            required>
+        <label>Mot de passe</label> <input type="password" maxlength="50" placeholder="Enter password" name="password" required>
         <br>
         <button type="submit" value="register" name="ok">S'inscrire</button>
     </form>
 
-<?php
-    if (isset($_POST["ok"]))
-    {
-        createAccount($_POST['id'], $_POST['gender'], $_POST['name'], $_POST['lastname'], $_POST['birthdate'], $_POST['email'], $_POST['password']);
-    }
-?>
+    <?php
+        if (isset($_POST["ok"]))
+        {
+            createAccount($_POST['id'], $_POST['gender'], $_POST['name'], $_POST['lastname'], $_POST['birthdate'], $_POST['email'], $_POST['password']);
+        }
+    ?>
 
 </body>
 
